8a1d6877b9e39eaffc5034ea728ab4a10f568b05
LudovicaPiro <32323247+LudovicaPiro@users.noreply.github.com>
2020-01-20 00:15:24 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // JavaScript Document
 
+//setTimeout(function(){jQuery('#dailyskins').fadeIn('show')}, 200);
+
   // navbar background color change on scroll
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();
@@ -12,68 +14,84 @@
     }
   })
 
-//Hover category btn//
+
 $(document).ready(function(){	
 
-// purchase 
+//cart btn click makes text appear
+        $(".cartbtn").click(
+                    function() {
 
-$(".purchasebtn").click( function() {
-  //close the cart
-  $("#mySidenav").css("width", "0");
- 
-  //open moralist page
-  $("#mySidemoral").css("width", "50%");
- 
+                        $("#cartcontent-cart").delay(500).fadeIn(500,"linear" );
+                    });
+// purchase click opens moralist page
+        $(".purchasebtn").click(
+                    function() {
 
- 
-});
+                        $(".cartcontent-moral").delay(500).fadeIn(500,"linear" );
+                    });
+        
+       $(".purchasebtn").click( 
+                    function() {
+                                 //close the cart
+                                 $("#mySidenav").css("width", "0");
+                                 $("#cartcontent-cart").fadeOut(500, "linear");
+                              
+                                 //open moralist page
+                                 $("#mySidemoral").css("width", "50%");
+                                 }
+                                 );
+//behind the scene button in moralist page fades text out
+      $(".moralbtn").click(
+                    function() {
+                      $(".cartcontent-moral").fadeOut(500, "linear");
+                    }
+                    )
 
-
-
-							 $(".whiteHover").hover(	
-							 							function(){$(".whiteHover").css({"color": "white", "border": "2px solid white" });
-							 										$(".categoryDescr").css("border-top-style", "none");
+// category color change on hover and background image change
+			 $(".whiteHover").hover(	
+			 							function(){$(".whiteHover").css({"color": "white", "border": "2px solid white" });
+			 										$(".categoryDescr").css("border-top-style", "none");
                                   $(".categoryLinks").css("color", "white");
-							 										},
-							 							function(){$(".whiteHover").css({"color": "#F39200", "border": "2px solid #F39200"});
-							 										$(".categoryDescr").css("border-top-style", "none");
+			 										},
+			 							function(){$(".whiteHover").css({"color": "#F39200", "border": "2px solid #F39200"});
+			 										$(".categoryDescr").css("border-top-style", "none");
                                   $(".categoryLinks").css("color", "#F39200");
-							 										},  											
-    										 		); 
-							 $("#MAKE").hover(	
-  												function(){	$("#CategoryContainer").addClass("CategoryImgFadeIn");
-  															$("#CategoryContainer").css("background-image", "url('assets/images/category01.jpg')")
-  														  },
-    											function(){	$("#CategoryContainer").removeClass("CategoryImgFadeIn");
-    														$("#CategoryContainer").css("background-image", "none")
-    														}
-    										 );
+			 										},  											
+    						 		); 
+			 $("#MAKE").hover(	
+  								function(){	$("#CategoryContainer").addClass("CategoryImgFadeIn");
+  											$("#CategoryContainer").css("background-image", "url('assets/images/category01.jpg')")
+  										  },
+    							function(){	$("#CategoryContainer").removeClass("CategoryImgFadeIn");
+    										$("#CategoryContainer").css("background-image", "none")
+    										}
+    						 );
 
-							 $("#LEAVE").hover(
-  												function(){$("#CategoryContainer").addClass("CategoryImgFadeIn");
-  															$("#CategoryContainer").css("background-image", "url('assets/images/category02.jpg')")
-  														  },
-    											function(){	$("#CategoryContainer").removeClass("CategoryImgFadeIn");
-    														$("#CategoryContainer").css("background-image", "none")
-    														}
-    										 );
+			 $("#LEAVE").hover(
+  								function(){$("#CategoryContainer").addClass("CategoryImgFadeIn");
+  											$("#CategoryContainer").css("background-image", "url('assets/images/category02.jpg')")
+  										  },
+    							function(){	$("#CategoryContainer").removeClass("CategoryImgFadeIn");
+    										$("#CategoryContainer").css("background-image", "none")
+    										}
+    						 );
 
-							 $("#SWIM").hover(
-  												function(){$("#CategoryContainer").addClass("CategoryImgFadeIn");
-  															$("#CategoryContainer").css("background-image", "url('assets/images/category03.jpg')")
-  															},
-    											function(){$("#CategoryContainer").removeClass("CategoryImgFadeIn");
-    														$("#CategoryContainer").css("background-image", "none")
-    														}
-    										 );
-							 $("#FEEL").hover(
-  												function(){$("#CategoryContainer").addClass("CategoryImgFadeIn");
-  															$("#CategoryContainer").css("background-image", "url('assets/images/category04.jpg')")
-  															},
-    											function(){$("#CategoryContainer").removeClass("CategoryImgFadeIn");
-    														$("#CategoryContainer").css("background-image", "none")
-    														}
-    										 );
+			 $("#SWIM").hover(
+  								function(){$("#CategoryContainer").addClass("CategoryImgFadeIn");
+  											$("#CategoryContainer").css("background-image", "url('assets/images/category03.jpg')")
+  											},
+    							function(){$("#CategoryContainer").removeClass("CategoryImgFadeIn");
+    										$("#CategoryContainer").css("background-image", "none")
+    										}
+    						 );
+			 $("#FEEL").hover(
+  								function(){$("#CategoryContainer").addClass("CategoryImgFadeIn");
+  											$("#CategoryContainer").css("background-image", "url('assets/images/category04.jpg')")
+  											},
+    							function(){$("#CategoryContainer").removeClass("CategoryImgFadeIn");
+    										$("#CategoryContainer").css("background-image", "none")
+    										}
+    						 );
 });
 
 
@@ -101,12 +119,15 @@ function closeNav() {
   $("#mySidenav").css("width", "0");
   $("#main-content, #footer, #nav-content").css("opacity", "1");
   $("#body").css("background-color", "black");
+  $("#cartcontent-cart").fadeOut(100, "linear");
+
 }
 
 function closeMoral() {
   $("#mySidemoral").css("width", "0");
   $("#main-content, #footer, #nav-content").css("opacity", "1");
-  $("#body, #nav-content").css("background-color", "black");
+  $("#body").css("background-color", "black");
+  $(".cartcontent-moral").fadeOut(100, "linear");
 }
 
 function openNav() {
